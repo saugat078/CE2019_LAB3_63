1a2ca5b0f712b7581b42c7ba58573a66eb2063e7
saugat poudel <saugat.poudel478@gmail.com>
2021-07-19 11:47:13 +0545

    LinkedListBST header file

---
+++ LLBST.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "BST.h"
+using namespace std;
+
+class Node{
+	public:
+	int key;
+	Node *left;
+	Node *right;
+};
+
+class node{
+    public:
+        Node* info;
+        node* next;
+};
+
+class LinkedBST: public BST{
+	private:
+	Node *root = NULL;
+	public:
+	void preorder();
+	void inorder();
+	void add(int data);
+	bool search(int data);
+	bool isEmpty();
+	int max();
+	void inorder(Node* root);
+	void remove_node();
+	Node* remove_node(Node *root,int key);
+	Node* get_root(){
+			return root;
+		}
+};
+
+class Stack{
+    public:
+    Stack();
+    ~Stack();
+    
+    void push(Node* a);
+    Node* pop();
+    bool isEmpty();
+    bool isFull();
+    int peak();
+    
+    private:
+    node* stack;
+    node* top;
+};
