ca32db3cd96424123c9535897b2b2ca51cf11e44
saugat poudel <saugat.poudel478@gmail.com>
2021-07-20 20:38:10 +0545

    BST header file

--- BST.h
+++ BST.h
@@ -1,7 +1,8 @@
 #ifndef BST_h
 #define BST_h
-class BST{
+class AbstractBST{
 		public:
+			virtual bool isEmpty()=0;
 			virtual void add(int data)=0;
 			virtual bool search(int data)=0;
 			virtual int max()=0;
